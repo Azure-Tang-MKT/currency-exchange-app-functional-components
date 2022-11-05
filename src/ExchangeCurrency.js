75abc549dda2e0191fd27ef283306b90bc719ab8
apple <apple@192.168.0.104>
2022-11-05 13:24:39 +0800

    update foreign currency convertor

--- src/ExchangeCurrency.js
+++ src/ExchangeCurrency.js
@@ -1,40 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { Select, message, InputNumber } from "antd";
+import { Select, message, InputNumber, DatePicker, Spin } from "antd";
 import moment from "moment";
 import "antd/dist/antd.css";
 import "./App.css";
 
 const { Option } = Select; //
 
-// state = {
-//   selectedCurrency: "CNY",
-//   exchangeRate: "",
-//   date: moment().format("YYYY-MM-DD"), //moment js
-//   inputAmount: "",
-//   amountinCAD: "",
-// };
-
-const ExchangeCurrency = (props) => {
+const ExchangeCurrency = () => {
+  <Spin />;
   const [SelectedCurrency, setSelectedCurrency] = useState("CNY");
   const [exchangeRate, setexchangeRate] = useState("");
   const [date, setdate] = useState(moment().format("YYYY-MM-DD"));
-  const [inputAmount, setinputAmount] = useState("");
+  const [amount, setamount] = useState("");
+  // const [CADAmount, setCADAmount] = useState("");
+  const [changeforeignAmount, setchangeforeignAmount] = useState("true");
+  const [isLoading, setisLoading] = useState("true");
+
+  const handleDateChange = (date) => {
+    setdate(moment(date).format("YYYY-MM-DD"));
+  };
 
   const changeCurrency = (value) => {
     setSelectedCurrency(value);
   };
 
-  const changeAmount = (amount) => {
-    setinputAmount(amount);
-    // setamountinCAD(amount * this.state.exchangeRate);
+  let foreignAmount;
+  let cadAmount;
+  if (changeforeignAmount) {
+    foreignAmount = amount;
+    cadAmount = amount / exchangeRate;
+  } else {
+    cadAmount = amount;
+    foreignAmount = amount * exchangeRate;
+  }
+
+  const handleforeignChange = (e) => {
+    setamount(e);
+    setchangeforeignAmount(true);
+  };
+  const handlecadChange = (e) => {
+    setamount(e);
+    setchangeforeignAmount(false);
   };
 
   const fetchData = async () => {
+    setisLoading(true);
     try {
       const res = await fetch(
-        `https://www.bankofcanada.ca/valet/observations/FXCAD${SelectedCurrency}/json?start_date=${
-          props.date ? props.date : date
-        }&end_date=${props.date ? props.date : date}`
+        `https://www.bankofcanada.ca/valet/observations/FXCAD${SelectedCurrency}/json?start_date=${date}&end_date=${date}`
       );
       const data = await res.json();
       console.log(data);
@@ -45,35 +58,38 @@ const ExchangeCurrency = (props) => {
         message.info("There is no exchange rate on this date!!");
         setexchangeRate("");
       }
+      setisLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  let cadAmount;
-  cadAmount = inputAmount / exchangeRate;
-  console.log(cadAmount);
-
   useEffect(() => {
     fetchData();
-  }, [SelectedCurrency, props.date]);
+  }, [SelectedCurrency, date]);
 
   return (
-    <div className="convertorPart">
-      <div className="text">
-        <p>Amount of Foreign Currency</p>
-        <p className="currencies">Currencies</p>
-      </div>
-      <div className="foreignPortion">
+    <div>
+      <div className="title">Currency Convertor</div>
+      <div className="text-date">Date</div>
+
+      <DatePicker onChange={(date) => handleDateChange(date)} />
+      {isLoading && <Spin />}
+      <div className="convertorPart">
+        <p className="text">Amount of Foreign Currency</p>
+        <p className="text">Currencies</p>
+
         <div className="inputAmount">
-          <InputNumber inputAmount onChange={changeAmount} />
+          <InputNumber
+            type="number"
+            placeholder="Type amount"
+            // inputAmount
+            value={foreignAmount}
+            onChange={handleforeignChange}
+          />
         </div>
         <div className="currencyOptions">
-          <Select
-            defaultValue="CNY"
-            style={{ width: 130 }}
-            onChange={changeCurrency}
-          >
+          <Select defaultValue="CNY" onChange={changeCurrency}>
             <Option value="CNY">CNY</Option>
             <Option value="USD">USD</Option>
             <Option value="EUR">EUR</Option>
@@ -84,17 +100,25 @@ const ExchangeCurrency = (props) => {
             <Option value="HKD">HKD</Option>
           </Select>
         </div>
-      </div>
-      <div className="cadPortion">
+        <p className="text">Amount of Canadian Dollars</p>
+        <p className="text">Canadian Dollar</p>
         <div className="cadAmount">
-          <InputNumber value={cadAmount} />
+          <InputNumber
+            type="number"
+            placeholder="Type amount"
+            value={cadAmount}
+            onChange={handlecadChange}
+          />
         </div>
         <div className="CAD">
-          <Select defaultValue="CAD" style={{ width: 130 }}></Select>
+          <Select defaultValue="CAD"></Select>
         </div>
-      </div>
-      <div style={{ marginTop: "10px" }}>
-        <p>The exchange rate is ${exchangeRate}</p>
+
+        <div className="setence">
+          <p>
+            The exchange rate is <b>${exchangeRate}</b>
+          </p>
+        </div>
       </div>
     </div>
   );
